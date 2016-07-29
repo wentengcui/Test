3db81847546c6e622b19e7a9ee990d26827fd55d
wentengcui <wenteng_cui@163.com>
2016-07-29 21:10:08 +0800

    first version

--- Spring/src/main/java/com/dendi/entity/Wenming.java
+++ Spring/src/main/java/com/dendi/entity/Wenming.java
@@ -4,7 +4,7 @@ public class Wenming {
 	public static int i = 6;
 	public static int j = 8;
 	public static void main(String[] args) {
-		int m  = i+j;
+		int m    = i+j;
 		int n = i+j;
 		System.out.println("i+j="+m);
 	}
