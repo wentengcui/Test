226a960187455908efc3d383271109a75e3f819e
Hidden <Hidden>
2016-07-29 21:03:32 +0800

    hello！majian

---
+++ Spring/src/main/java/com/dendi/controller/TestMajian.java
@@ -0,0 +1,10 @@
+package com.dendi.controller;
+
+public class TestMajian {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("欢迎我来到github");
+	}
+
+}
