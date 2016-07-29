290c865d778bb29b748dfe5e511fbf84809ae88e
bmxly zzty <bmxly zzty>
2016-07-29 20:39:18 +0800

    test

--- Spring/src/main/java/com/dendi/controller/Test.java
+++ Spring/src/main/java/com/dendi/controller/Test.java
@@ -43,6 +43,7 @@ public class Test {
 	public String addStudent1( Student student, ModelMap model) {
 		System.out.println("addStuden1t");
 	System.out.println(student.getName());
+	System.out.println(student.getName());
 		return "success";
 	}
 }
