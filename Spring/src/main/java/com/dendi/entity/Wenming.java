c424685f303ffc835f163866ba7398ef0e69be76
wentengcui <wenteng_cui@163.com>
2016-07-29 21:01:40 +0800

    July29th, first version

---
+++ Spring/src/main/java/com/dendi/entity/Wenming.java
@@ -0,0 +1,9 @@
+package com.dendi.entity;
+
+public class Wenming {
+	public static int i = 6;
+	public static int j = 8;
+	public static void main(String[] args) {
+		System.out.println("i+j="+(i+j));
+	}
+}
